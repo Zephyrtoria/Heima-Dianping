0ee93fb876c82ad4fea2a9ab76d33da41c32e5f2
Zephyrtoria <2594379136@qq.com>
2025-03-17 14:21:57 +0800

    init

---
+++ src/main/java/github/zephyrtoria/heima_dianping/HeimaDianpingApplication.java
@@ -0,0 +1,13 @@
+package github.zephyrtoria.heima_dianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimaDianpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeimaDianpingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/zephyrtoria/heima_dianping/HeimaDianpingApplicationTests.java
@@ -0,0 +1,13 @@
+package github.zephyrtoria.heima_dianping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimaDianpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
